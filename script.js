797e53ca7f3cd81ca7973f0ee21e8478c706cbf9
TalhaShaikh150 <tssmart233@gmail.com>
2025-07-14 17:12:14 +0500

    Implemented Sunrise and Sunset Display Container And Made It Interactive

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const weatherImageElement = document.querySelector(".weather-img");
 const feelsLikeTemp = document.querySelector(".feels-like-temp");
 const uvIndex = document.querySelector(".uv-index");
 const humidity = document.querySelector(".humidity");
-
+const lastTime = document.querySelector(".last-time");
 //For Highlights Section
 
 const wind = document.querySelector(".wind");
@@ -15,33 +15,30 @@ const visibility = document.querySelector(".visibility");
 const pressure = document.querySelector(".pressure");
 const dewPoint = document.querySelector(".dew-point");
 
+//Sunrise and Sunset
+
+const sunriseElement = document.querySelector(".sunrise");
+const sunsetElement = document.querySelector(".sunset");
+
 //Previous Theme
 
 let previosTheme = "";
 //Api Code
 const key = "5eccd56d9ce745919e2152215251307";
-let city = "islamabad";
+let city = "karachi";
 
 function fetchData() {
   fetch(
-    `https://api.weatherapi.com/v1/forecast.json/current.json?key=${key}&q=${city}&aqi=yes`
+    `https://api.weatherapi.com/v1/current.json/forecast.json?key=${key}&q=${city}&aqi=yes`
   )
     .then((res) => res.json())
     .catch(() => {})
     .then((data) => {
-      // cityName.innerHTML = data.location.name;
-      // countryName.innerHTML = data.location.country;
-      // temperature.innerHTML = Math.round(data.current.temp_c) + "°";
-      // weatherStatus.innerHTML = data.current.condition.text;
-      // weatherDetail.innerHTML = data.current.condition.text;
-      // feelsLikeTemp.innerHTML = Math.round(data.current.feelslike_c) + "°";
-      // let weatherIcon = `${data.current.condition.text.toLowerCase()}`;
-
-      // weatherImage.src = `assets/${weatherIcon}.png`;
       uvRaysRange(data);
       renderWeatherData(data);
       showWheatherImage(data);
       searchCity(data);
+      console.log(data);
     });
 }
 fetchData();
@@ -66,9 +63,10 @@ function searchCity(data) {
         alert("Please Fill The City Input");
         return;
       }
+      city = searchCityInput.value;
       uvRaysRange(data);
       showWheatherImage(data);
-      city = searchCityInput.value;
+
       fetchData();
     }
   });
@@ -79,14 +77,24 @@ function renderWeatherData(data) {
   countryName.innerHTML = data.location.country;
   temperature.innerHTML = Math.round(data.current.temp_c) + "°";
   weatherStatus.innerHTML = data.current.condition.text;
-  weatherDetail.innerHTML = data.current.condition.text;
   feelsLikeTemp.innerHTML = Math.round(data.current.feelslike_c) + "°";
   humidity.innerHTML = data.current.humidity + "%";
   wind.innerHTML = Math.round(data.current.wind_kph) + " km/h";
   visibility.innerHTML = Math.round(data.current.vis_km) + " km/h";
+  let timestampInSeconds = data.current.last_updated_epoch;
+  const date = new Date(timestampInSeconds * 1000);
 
+  lastTime.innerHTML = date.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
   pressure.innerHTML = data.current.pressure_mb + " mb";
   dewPoint.innerHTML = data.current.dewpoint_c + "°";
+
+  //  = data.forcast;
+  sunriseElement.innerHTML = data.forecast.forecastday[0].astro.sunrise;
+  sunsetElement.innerHTML = data.forecast.forecastday[0].astro.sunset;
 }
 
 function uvRaysRange(data) {
@@ -102,13 +110,21 @@ function uvRaysRange(data) {
     uvIndex.style.color = "#ff3939ff";
     uvIndex.innerHTML = uv + " Very High";
   } else if (uv >= 8 && uv <= 11) {
-    uvIndex.style.color = "#ea0d0dff";
+    uvIndex.style.color = "#ea4242ff";
     uvIndex.innerHTML = uv + " Extreme";
   }
 }
 
 function checkTheme(rootElement) {
-  const themes = ["overcast", "rain", "thunder", "clear", "sunny"];
+  const themes = [
+    "overcast",
+    "rain",
+    "thunder",
+    "clear",
+    "sunny",
+    "snowy",
+    "default",
+  ];
   themes.forEach((theme) => {
     if (rootElement.classList.contains(theme)) {
       rootElement.classList.remove(theme);
@@ -119,6 +135,7 @@ function checkTheme(rootElement) {
 function showWheatherImage(data) {
   const rootElement = document.documentElement;
   checkTheme(rootElement);
+  let weatherMessage = "";
 
   let conditionText = data.current.condition.text;
   let weatherImage = "default";
@@ -126,27 +143,28 @@ function showWheatherImage(data) {
     weatherImage = "clear.png";
     previosTheme = "clear";
     rootElement.classList.add(previosTheme);
+    weatherMessage = "☀️ Clear skies today. Enjoy the sunshine!";
   } else if (conditionText === "Sunny") {
     weatherImage = "sunny.png";
     previosTheme = "sunny";
     rootElement.classList.add(previosTheme);
+    weatherMessage = "☀️ It's clear and sunny. A perfect day to be outdoors.";
   } else if (
+    conditionText === "Mist" ||
+    conditionText === "Partly cloudy" ||
     conditionText === "Partly Cloudy" ||
     conditionText === "Cloudy" ||
     conditionText === "Overcast"
   ) {
     previosTheme = "overcast";
     rootElement.classList.add(previosTheme);
-
     weatherImage = "cloudy.png";
-  } else if (
-    conditionText === "Mist" ||
-    conditionText === "Fog" ||
-    conditionText === "Freezing fog"
-  ) {
+    weatherMessage = "☁️ It's cloudy.enjoy The Weather.";
+  } else if (conditionText === "Fog" || conditionText === "Freezing fog") {
     weatherImage = "fog.png";
     previosTheme = "overcast";
     rootElement.classList.add(previosTheme);
+    weatherMessage = "🌫️ Drive carefully. Visibility is low due to fog.";
   } else if (
     conditionText === "Patchy rain possible" ||
     conditionText === "Patchy rain nearby" ||
@@ -162,6 +180,9 @@ function showWheatherImage(data) {
   ) {
     weatherImage = "rain.png";
     previosTheme = "rain";
+    weatherMessage =
+      "🌦️ Rainy weather. Drive safely and watch for slippery roads.";
+
     rootElement.classList.add(previosTheme);
   } else if (
     conditionText === "Heavy rain at times" ||
@@ -170,6 +191,8 @@ function showWheatherImage(data) {
     weatherImage = "heavy-rain.png";
     previosTheme = "thunder";
     rootElement.classList.add(previosTheme);
+    weatherMessage =
+      "🌧️ It's pouring! Stay indoors if possible and avoid waterlogged areas.";
   } else if (
     conditionText === "Thundery outbreaks possible" ||
     conditionText === "Thundery outbreaks in nearby" ||
@@ -180,6 +203,8 @@ function showWheatherImage(data) {
   ) {
     weatherImage = "thunder.png";
     previosTheme = "thunder";
+    weatherMessage =
+      "⚡ Stay indoors and avoid open areas during thunderstorms.";
     rootElement.classList.add(previosTheme);
   } else if (
     conditionText === "Patchy snow possible" ||
@@ -195,6 +220,9 @@ function showWheatherImage(data) {
     conditionText === "Blizzard"
   ) {
     weatherImage = "snow.png";
+    previosTheme = "snowy";
+    rootElement.classList.add(previosTheme);
+    weatherMessage = "🧊 It's freezing. Bundle up and keep warm.";
   } else if (
     conditionText === "Patchy sleet possible" ||
     conditionText === "Light sleet" ||
@@ -210,14 +238,29 @@ function showWheatherImage(data) {
     conditionText === "Light freezing rain" ||
     conditionText === "Moderate or heavy freezing rain"
   ) {
+    previosTheme = "snowy";
+    rootElement.classList.add(previosTheme);
+    weatherMessage = "🧊 It's snowing. Bundle up and keep warm.";
+
     weatherImage = "ice.png";
   } else if (
     conditionText === "Patchy light drizzle" ||
     conditionText === "Light drizzle"
   ) {
     weatherImage = "drizzle.png";
+    previosTheme = "rain";
+    weatherMessage =
+      "🌦️ Light drizzle outside. Keep an umbrella handy just in case.";
+
+    rootElement.classList.add(previosTheme);
+  } else {
+    previosTheme = "default";
+
+    rootElement.classList.add(previosTheme);
   }
 
+  weatherDetail.innerHTML = weatherMessage;
+
   // Apply image
-  weatherImageElement.src = `/assets/${weatherImage}`;
+  weatherImageElement.src = `assets/${weatherImage}`;
 }
